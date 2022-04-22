55332328a325320a9e9d0b245b3a83fcdb00ab90
chenhui7373 <sunw31@163.com>
2022-04-22 14:03:13 +0800

    a11y support

--- component-vue-ovd/src/main.js
+++ component-vue-ovd/src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue';
-import App from './App.vue'
-import store from './store.js';
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store.js";
 
-createApp(App).use(store).mount('#app')
+createApp(App).use(store).mount("#app");
+
+module.exports = {
+  addons: ["@storybook/addon-a11y"],
+};
